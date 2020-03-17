be89e8864edf0def8f8d14d737b13b6252dad7e1
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-17 14:20:29 +0100

    Profile for approx 100 secs

--- profile.R
+++ profile.R
@@ -3,7 +3,10 @@ library(beautier)
 filename <- tempfile()
 
 utils::Rprof(filename)
-check_inference_model(create_test_inference_model())
+for (i in seq_len(1000)) {
+  check_inference_model(create_test_ns_inference_model())
+  check_inference_model(create_test_inference_model())
+}
 utils::Rprof(NULL)
 print(utils::summaryRprof(filename))
 
